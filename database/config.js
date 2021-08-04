184ced6c4af99f77bdb8e50532dd7eedd65a96ca
Ernesto <ernestosanchezinfante@gmail.com>
2021-08-03 19:59:55 -0500

    CORS y Express -primer commit

---
+++ database/config.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+const dbConnection = async () => {
+
+    try {
+
+        mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex:true
+        });
+
+        console.log('DB Online');
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hpra de iniciar la BD ver logs');
+    }
+
+}
+
+
+module.exports={
+    dbConnection
+}
